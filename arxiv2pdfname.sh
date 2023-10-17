d5567f4c1066f47a3ef4f4b5851bf0f4d698d993
Roland Herzog <roland.herzog@iwr.uni-heidelberg.de>
2023-10-17 17:09:29 +0200

    Adjust arxiv2pdfname.sh to reflect change in source code returned by arXiv

--- arxiv2pdfname.sh
+++ arxiv2pdfname.sh
@@ -23,16 +23,14 @@ curl -sL http://arxiv.org/abs/$1 > $tmpfile
 titlepattern="<meta name=\"citation_title\" content=\"([^\"]+)\""
 title=$( perl -ne "/$titlepattern/ and print \$1" < $tmpfile | awk '{gsub(" ","_"); gsub("/","_"); gsub("&#x27;","\x27"); gsub("&#39;","\x27"); gsub(":","_"); gsub("-[-]+","-"); gsub("_[_]+","_"); print}')
 
-# Get authors
-authorpattern="<meta name=\"citation_author\" content=\"([^\"]+),.*\""
-author=$( perl -ne "/$authorpattern/ and print \$1 . \"_\"" < $tmpfile)
+# Get authors.
+# The pattern looks like this:
+# <meta name="citation_author" content="Family Names, Given Names" />
+# We non-greedily capture everything following <meta name="citation_author" content="
+# and before a comma, then replace the newlines between matches by underscores.
+author=$(grep -o -P '(?<=<meta name="citation_author" content=")(.*?)(?=,)' < $tmpfile | tr '\n' '_')
+# Replace space, backslash, and apostrophe by nothing.
 author=$(echo $author | awk '{ gsub ("[ '\'']", "", $0); print}');
-# Remove spaces
-# Uppercase initial letter and everything after a space
-# author=$(echo $author |\
-# 	perl -nE '$_ = lc($_); say join " ", map {ucfirst $_} split /\s/' );
-# author=$(echo $author |\
-# 	awk '{ gsub (" ", "", $0); print}');
 # echo $author
 
 # Finalize suggested file name
